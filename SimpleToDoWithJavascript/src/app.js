e1e4164d93e0f6eafc9a5951fe0a6d051bd1cf04
marioszocs <szocs.mario@gmail.com>
2020-12-13 14:24:12 +0100

    Update

--- SimpleToDoWithJavascript/src/app.js
+++ SimpleToDoWithJavascript/src/app.js
@@ -1,33 +1,29 @@
-// Vue.createApp({
-//   data() {
-//     return {
-//       goals: [],
-//       enteredValue: ''
-//     };
-//   },
-//   methods: {
-//     addGoal() {
-//       this.goals.push(this.enteredValue);
-//       this.enteredValue = '';
-//     }
-//   }
-// }).mount('#app');
-
-//Egyetlen button a html oldalon
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
-
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
-
-//a buttonEl-hez hozzaadunk egy listenert 
-buttonEl.addEventListener('click', addGoal);
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app');
 
 
-//Kod formazas CTRL + SHIFT + F
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
